272efd67d98044d54c41187171eb03b248341fa9
Peter Jang <peter.x.jang@gmail.com>
2025-04-02 14:35:35 -0500

    Add card hover styling

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -4,7 +4,10 @@ export function ProductsIndex({ products }) {
       <h1 className="text-3xl font-bold my-4">All products {products.length} total</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
         {products.map((product) => (
-          <div key={product.id} className="rounded shadow-lg mb-4">
+          <div
+            key={product.id}
+            className="rounded mb-4 transform transition duration-300 hover:scale-105 shadow-md hover:shadow-xl"
+          >
             <img src={product.primary_image_url} alt="" className="w-full aspect-[4/3] object-cover" />
             <div className="p-4">
               <h2 className="text-2xl font-bold">{product.name}</h2>
